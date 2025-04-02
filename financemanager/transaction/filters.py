a5d919276c561856a526b90db976c573a45c099d
delawer33 <shukry.aliev@mail.ru>
2025-04-02 11:49:38 +0300

    remade styles

--- financemanager/transaction/filters.py
+++ financemanager/transaction/filters.py
@@ -1,5 +1,6 @@
-from django_filters import FilterSet, DateFromToRangeFilter, Filter, OrderingFilter
+from django_filters import DateFilter, FilterSet, Filter, OrderingFilter
 from django_filters.widgets import RangeWidget
+from django.forms import DateInput
 from django.db.models import Q
 
 from .models import Transaction
@@ -23,7 +24,17 @@ class CategoryFilter(Filter):
 
 
 class TransactionFilter(FilterSet):
-    date = DateFromToRangeFilter(widget=RangeWidget(attrs={'type': 'date'}))
+    # date = DateFromToRangeFilter(widget=RangeWidget(attrs={'type': 'date'}))
+    date_from = DateFilter(
+        field_name='date', 
+        lookup_expr='gte', 
+        widget=DateInput(attrs={'type': 'date'})
+    )
+    date_to = DateFilter(
+        field_name='date', 
+        lookup_expr='lte', 
+        widget=DateInput(attrs={'type': 'date'})
+    )
     description = DescriptionFilter()
     category = CategoryFilter()
     sort_by = OrderingFilter(
